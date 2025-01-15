60e655ae54e15ba706b5d4d84431b57f9a54ed0e
jaralve <63028909+jaralve@users.noreply.github.com>
2025-01-15 15:46:52 +0800

    Update CORS settings

--- server/server.js
+++ server/server.js
@@ -19,7 +19,7 @@ app.use(express.json());
 // CORS config
 // const allowedOrigins = ['http://localhost:3000/'];
 const corsOptions = {
-  origin: process.env.CORS_ORIGIN || 'http://localhost:3000',
+  origin: process.env.CORS_ORIGIN || 'https://todos-fe.onrender.com/',
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE'],
   allowedHeaders: ['Content-Type', 'Authorization'],
 };
