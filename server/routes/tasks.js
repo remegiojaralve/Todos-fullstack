ce297e9e49bef8f01e8163a28dc04bf692ac738b
jaralve <63028909+jaralve@users.noreply.github.com>
2025-01-16 15:03:06 +0800

    Fix error on tasks.js

--- server/routes/tasks.js
+++ server/routes/tasks.js
@@ -1,5 +1,5 @@
 const express = require('express');
-const Task = require('../models/task');
+const Task = require('../models/Tasks');
 const router = express.Router();
 
 router.get("/tasks", async (req, res) => {
